a8dbde2993e1613beb5f9c2f11bedcad644214c6
Abid Saeed <abid.saeed@schuja.com>
2023-10-08 22:35:20 +0500

    vite issue fixed & bug fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Question;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::factory()
+            ->has(Question::factory()->count(3))
+            ->create();
     }
 }
